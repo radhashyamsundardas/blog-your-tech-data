31d2b532ead5252771bdfa704a60ef6380b2c354
hiren patel <hirenpatel0526@yahoo.com>
2022-10-25 07:01:43 -0700

    connection

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.blog_db,
+  process.env.root,
+ '',
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
+
+module.exports = sequelize;
