23e69d48ae11e02b3671003b51fd16dcc48bed33
hiren patel <hirenpatel0526@yahoo.com>
2022-10-25 14:15:49 -0700

    comment authorization js

--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,5 @@
 const auth = (req, res, next) => {
+    // authorizing if required session logged in or it will go to next page
     if (!req.session.loggedIn){
         res.redirect('/login')
     } else {
