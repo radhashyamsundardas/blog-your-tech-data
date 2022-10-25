fa3323b7545d52ab25bccc36ea4ed40d3567ba94
hiren patel <hirenpatel0526@yahoo.com>
2022-10-25 11:54:49 -0700

    confused

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const path = require('path');
 const express = require('express');
+// Import express-session
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+// const helpers = require('./utils');
 
 const app = express();
 const PORT = process.env.PORT || 3003;
 
-const hbs = exphbs.create({});
+// Set up sessions
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,8 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(routes);
 
-
-  app.listen(PORT, () => {
-    console.log(`Now now lisening on port ${PORT}`);
-    sequelize.sync({ force: false });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
