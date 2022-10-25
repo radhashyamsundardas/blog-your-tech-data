570b97c7fffc55a9007956535f38a8f9928570da
hiren patel <hirenpatel0526@yahoo.com>
2022-10-25 16:22:02 -0700

    tryin serverjs

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require('express');
 // Import express-session
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
-// const helpers = require('./utils');
+const sequelizesession = require()
 
 const app = express();
 const PORT = process.env.PORT || 3001;
