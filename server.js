e061839f44a32774c0985da485f9c6950b528449
hiren patel <hirenpatel0526@yahoo.com>
2022-10-25 18:14:21 -0700

    finished setting up server.js

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
 const path = require('path');
 const express = require('express');
-// Import express-session
+
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-const sequelizesession = require()
+const sequelizestore = require('connect-session-sequelize')(session.store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up sessions
 
+
+const hbs = exphbs.create({helpers});
+const session = {
+  secret: 'Tech blog secret',
+  cookie: {},
+  resave: false,
+  saveUnintialized: true,
+  store: new sequelizeStore({
+    db.sequelize
+  })
+};
+
+app,use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-// });
+});
