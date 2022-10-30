4905cb35b8a42e1dae7ee4077d96f3f074dc6054
hiren patel <hirenpatel0526@yahoo.com>
2022-10-30 07:24:38 -0700

    finished get

--- controllers/dashroutes.js
+++ controllers/dashroutes.js
@@ -2,3 +2,38 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const {comment, post, user} = require('../models');
 const withAuth = require('../utils/auth');
+
+
+router.get('/', withAuth, (req,res) => {
+    post.findAll({
+        where: {
+            user_id: request.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_by'
+        ],
+        includes: [{
+            model: comment,
+            attributes: ['id','user_id', 'post_id', 'created_by', 'comment'],
+            includes:{
+                model: user,
+                attributes: ['username']
+            }
+        }]
+        .then (postData => {
+            const post = postData.map(post => post.get({plain:true}));
+            res.render('dashboard', {posts,loggedIn:true});
+        })
+        .catch(error =>{
+            console.log(error);
+            res.status(500).json(err);
+        });
+
+
+
+
+    })
+})
