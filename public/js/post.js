00373234e1634ee5784722a9c2bc879d7cb404d8
hiren patel <hirenpatel0526@yahoo.com>
2022-11-01 04:06:51 -0700

    post javascript done

---
+++ public/js/post.js
@@ -0,0 +1,28 @@
+const { application, response } = require("express");
+const { json } = require("sequelize");
+
+async function newFormHandler (event){
+    event.preventDefault();
+
+    const title = document.getElementById('input[name="post-title"]').value;
+    const info = document.getElementById('input[name="info"]')
+
+    const answer = await fetch ('/api/postroute', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers:{
+            'contetn-type': 'application.json'
+        }
+    });
+    if (response.ok){
+        document.location.replace('/dashboard');
+    } else{
+        alert('add post didnt work');
+    }
+
+};
+
+document.getElementById('#new-post-form').addEventListener('submit', newFormHandler);
