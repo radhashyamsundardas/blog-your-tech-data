6e2b222c6f20c25bc0bc4a05f06f50708da370c9
hiren patel <hirenpatel0526@yahoo.com>
2022-11-02 12:08:04 -0700

    userroute

--- controllers/api/userroute.js
+++ controllers/api/userroute.js
@@ -18,6 +18,42 @@ router.post ('/', async(req,res) =>{
             res.status(500).json(err);
         }
     });
+
+    router.post ('/login', async(req,res) =>{
+        try{
+            const user = await User.findOne({
+                where: {
+                    username: req.body.username
+                }
+            });
+
+            if (!user){
+                res.status(400).json({message: 'no account found'});
+                return;
+            }
+            const password = user.checkPassword(req.body.password);
+
+            if(!password){
+                res.status(400).json({message: 'no account found'});
+                return;
+            }
+            
+                        req.session.save(() => {
+                            req.session.userId = user.id;
+                            req.session.username = user.username;
+                            req.session.loggedIn = true;
+                    
+                            res.json({user, message: 'logged in now'});
+                        });
+                    } catch (err){
+                            res.status(400).json({message:'no account found'});
+                        }
+                    });
+    
+ 
+
+
+
     
 
 
