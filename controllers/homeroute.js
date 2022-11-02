2857784832d51ca7a879f247b80d3317cb0d6f5f
hiren patel <hirenpatel0526@yahoo.com>
2022-11-02 12:58:05 -0700

    getrequest done

--- controllers/homeroute.js
+++ controllers/homeroute.js
@@ -1,1 +1,15 @@
 const router = require('express').Router();
+const {Post, Comment, User} = require('../models/');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req,res) => {
+    try{
+        const putData = await Post.findAll({
+            include:[User],
+        });
+        const posts = putData.map((post) => post.get({plain:true}));
+        res.render('all-posts-admin', {posts, loggedIn: req.session.logeedIn});
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
