0a24086eda4c1ca238e8d41969d0941242830a2d
hiren patel <hirenpatel0526@yahoo.com>
2022-11-02 13:25:52 -0700

    done

--- controllers/homeroute.js
+++ controllers/homeroute.js
@@ -38,3 +38,22 @@ router.get('/post/:id',withAuth, async (req,res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/login',  (req,res) => {
+      if (req.session.logeedIn){
+        res.redirect('/dashboard');
+        return;
+      }
+      res.render('login');
+});
+
+router.get('./signup', (req,res) =>{
+    if (req.session.logeedIn){
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('signup')
+});
+
+
+module.exports = router;
