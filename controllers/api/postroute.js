e5424f88390c1de9638902efae50c8154321d0ab
hiren patel <hirenpatel0526@yahoo.com>
2022-11-01 19:53:54 -0700

    finishedpostroute

--- controllers/api/postroute.js
+++ controllers/api/postroute.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const {post} = require('../../models');
 const withAuth = require('../../utils/auth');
+
+
+
 // post request to create new post
 router.post('/', withAuth, async(req,res) =>{
 const body = req.body;
@@ -13,5 +16,46 @@ try{
     console.log('failed', err);
     res.status(500).json(err);
 }
-
 });
+
+
+router.put ('./:id', withAuth, async(req,res) => {
+    try{
+        console.log("req.body");
+        const [rows] = awaitpost.update(req.body,{
+            where:{
+                id:req.params.id,
+            },
+        });
+        if(rows > 0){
+            res.status(200).end();
+        } else{
+            res.status(404).end();
+        }
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
+
+router.delete ('./:id', withAuth, async(req,res) => {
+    try{
+        console.log("req.body");
+        const [rows] = post.destroy({
+            where:{
+                id:req.params.id,
+            },
+        });
+        if(rows > 0){
+            res.status(200).end();
+        } else{
+            res.status(404).end();
+        }
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
+
+
+
